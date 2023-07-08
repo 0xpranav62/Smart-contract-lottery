81f77630164574ee2c83a14c2498fe503aba6516
0xpranav62 <0xpranavsharma@gmail.com>
2023-07-09 04:21:50 +0530

    Raffle contract Updated

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -151,4 +151,11 @@ contract Raffle is VRFConsumerBaseV2 {
     function getLastTimeStamp() external view returns(uint256) {
       return s_lastTimeStamp;
     }
+    function getRaffleState() external view returns(RaffleState) {
+      return s_raffleState;
+    }
+
+    function getPlayer(uint256 index) external view returns(address){
+      return s_players[index];
+    }
 }
